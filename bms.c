92719c619e48bceefee67099879081d0fd6cb08c
ivirant <izak.virant@students.vegova.si>
2026-07-12 16:50:05 +0200

    bms.c: fix vmin/vmax BMS current-limit comparisons using wrong struct field
    bms_update_limits() was comparing m_stat_vcell_min.soc / m_stat_vcell_max.soc
    (a 0-100% state-of-charge value) against m_conf.vmin_limit_start/vmax_limit_start
    (voltage thresholds) - nonsensical since these are different scales. Switched
    to .v_cell_min/.v_cell_max, the actual per-cell voltage fields. Also tightened
    the hysteresis margin on all four comparisons from 0.1 to 0.001, matching
    upstream - makes sense once the comparison is against a cell voltage instead
    of a percentage.

--- bms.c
+++ bms.c
@@ -423,11 +423,11 @@ void bms_update_limits(float *i_in_min, float *i_in_max,
 	float i_in_max_bms_vmin = i_in_max_conf;
 	if ((m_conf.limit_mode >> 2) & 1) {
 		if (m_stat_vcell_min.id >= 0) {
-			float vmin = m_stat_vcell_min.soc;
+			float vmin = m_stat_vcell_min.v_cell_min;
 
-			if (vmin > (m_conf.vmin_limit_start - 0.1)) {
+			if (vmin > (m_conf.vmin_limit_start - 0.001)) {
 				// OK
-			} else if (vmin < (m_conf.vmin_limit_end + 0.1)) {
+			} else if (vmin < (m_conf.vmin_limit_end + 0.001)) {
 				i_in_max_bms_vmin = 0.0;
 			} else {
 				i_in_max_bms_vmin = utils_map(vmin, m_conf.vmin_limit_start,
@@ -440,11 +440,11 @@ void bms_update_limits(float *i_in_min, float *i_in_max,
 	float i_in_min_bms_vmax = i_in_min_conf;
 	if ((m_conf.limit_mode >> 3) & 1) {
 		if (m_stat_vcell_max.id >= 0) {
-			float vmax = m_stat_vcell_max.soc;
+			float vmax = m_stat_vcell_max.v_cell_max;
 
-			if (vmax < (m_conf.vmax_limit_start + 0.1)) {
+			if (vmax < (m_conf.vmax_limit_start + 0.001)) {
 				// OK
-			} else if (vmax > (m_conf.vmax_limit_end - 0.1)) {
+			} else if (vmax > (m_conf.vmax_limit_end - 0.001)) {
 				i_in_min_bms_vmax = 0.0;
 			} else {
 				i_in_min_bms_vmax = utils_map(vmax, m_conf.vmax_limit_start,
